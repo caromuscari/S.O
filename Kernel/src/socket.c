ddadf5fb6e607a2f546865b063bce42e9d4a66a7
CeciliaLuna <cecilialuna.94@hotmail.com>
2017-04-09 17:55:48 -0300

    sockets

--- Kernel/src/socket.c
+++ Kernel/src/socket.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <commons/string.h>
 
 //extern int error_recepcion;
@@ -20,7 +21,7 @@ int iniciar_socket_cliente(char *ip, int puerto/*char *puerto*/)
 	}
 	printf("socket creado \n");
 
-	puerto_conexion = puerto;
+	puerto_conexion = /*atoi*/(puerto);
 
 	dest.sin_family = AF_INET;
 	dest.sin_port = htons( puerto_conexion );
@@ -29,14 +30,14 @@ int iniciar_socket_cliente(char *ip, int puerto/*char *puerto*/)
 	//Connecting socket
 	if (connect(connected_socket, (struct sockaddr*) &dest, sizeof(dest)) != 0) {
 		printf("error conectando socket\n");
-		//error_conectando = 1;
+	//	error_conectando = 1;
 	}else
 	printf("conectado a servidor %s:%d\n", ip, puerto_conexion);
 
 	return connected_socket;
 }
 
-int enviar(int socket_emisor, char *mensaje_a_enviar)
+int enviar_cl(int socket_emisor, char *mensaje_a_enviar)
 {
 	int ret;
 
@@ -54,7 +55,7 @@ int enviar(int socket_emisor, char *mensaje_a_enviar)
 	return ret;
 }
 
-char *recibir(int socket_receptor)
+char *recibir_cl(int socket_receptor)
 {
 	int ret;
 
@@ -69,7 +70,7 @@ char *recibir(int socket_receptor)
 				printf("error recibiendo el mensaje \n");
 
 			}
-			//error_recepcion = 1;
+//			error_recepcion = 1;
 			//close(socket_receptor);
 	}
 
@@ -80,6 +81,117 @@ char *recibir(int socket_receptor)
 	//free(sbuffer);
 
 }
+
+int iniciar_socket_server(char *ip, char *puerto)
+{
+	int socketServidor;
+	struct sockaddr_in my_addr;
+	int yes = 1;
+	int puerto_conexion;
+
+	//Creating socket
+	if ((socketServidor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("error creating socket\n");
+	}
+	//printf("created socket\n");
+
+	puerto_conexion = atoi(puerto);
+
+	setsockopt(socketServidor, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons( puerto_conexion );
+	my_addr.sin_addr.s_addr = inet_addr( ip );
+
+	//Binding socket
+	if (bind(socketServidor, (struct sockaddr*) &my_addr, sizeof(struct sockaddr_in)) != 0) {
+		perror("error binding socket\n");
+	}
+	//printf("binded socket\n");
+
+	return socketServidor;
+}
+
+int escuchar_conexiones(int socketServidor)
+{
+	int client_sock_accepted;
+	int c;
+	struct sockaddr_in client;
+	int BACKLOG = 20; //Cantidad de conexiones maximas
+
+	//Listening socket
+	if (listen(socketServidor, BACKLOG) != 0) {
+		perror("error listening\n");
+	}
+	//printf("listening at port: %d\n", ntohs(my_addr.sin_port));
+
+	//puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+
+	//accept connection from an incoming client
+	client_sock_accepted = accept(socketServidor, (struct sockaddr *)&client, (socklen_t*)&c);
+	if (client_sock_accepted < 0)
+	{
+		perror("error accepting failed\n");
+	    return 1;
+	}
+	//printf("Connection accepted\n");
+
+	return client_sock_accepted;
+}
+
+
+int enviar(int socket_emisor, char *mensaje_a_enviar, int *control)
+{
+	int ret;
+	signal(SIGPIPE, SIG_IGN);
+	size_t sbuffer = sizeof(char)*1024;
+	//char* buffer = (char*)malloc(sbuffer);
+
+	char *buffer = string_substring(mensaje_a_enviar,0,sbuffer);
+	//memcpy(buffer, mensaje_a_enviar, sbuffer);
+	*control = 0;
+
+	if ((ret = send(socket_emisor, buffer, sbuffer, MSG_NOSIGNAL)) < 0)
+	{
+		//close(socket_emisor);
+		//escribir_log("Se desconectó un entrenador");
+		*control = 1;
+		//perror("error en el envio del mensaje");
+	}
+
+
+	free(buffer);
+	return ret;
+}
+
+char *recibir(int socket_receptor, int *controlador)
+{
+	int ret;
+
+	char *buffer = malloc(1024);
+
+	*controlador = 0;
+
+	if ((ret = recv(socket_receptor, buffer, 1024, 0)) <= 0)
+	{
+		//printf("error receiving or connection lost \n");
+		if (ret == 0)
+		{
+			//printf("socket %d se desconectó \n", socket_receptor);
+		} else {
+			//printf("error recibiendo el mensaje \n");
+				}
+		*controlador = 1;
+		//close(socket_receptor);
+	}
+
+
+	//buffer[ret]='\0';
+	char *buffer_aux= strdup(buffer);
+	free(buffer);
+	return buffer_aux;
+}
+
 void cerrar_conexion(int socket_)
 {
 	close(socket_);
--- Kernel/src/socket.h
+++ Kernel/src/socket.h
@@ -1,18 +1,26 @@
-#ifndef SOCKET_H_
-#define SOCKET_H_
+#ifndef KERNEL_PRUEBAS_SRC_SOCKET_CLIENT_H_
+#define KERNEL_PRUEBAS_SRC_SOCKET_CLIENT_H_
 
 //Esta funcion genera un socket de tipo cliente (no puede aceptar conexiones, solo se conecta)
 int iniciar_socket_cliente(char *ip, int puerto/*char *puerto*/);
 
 //Esta funcion envia mensajes a traves del socket conectado
 //Devuelve un entero significativo de que si el mensaje fue enviado o no
-int enviar(int socket_emisor, char *mensaje_a_enviar);
+int enviar_cl(int socket_emisor, char *mensaje_a_enviar);
 
 //Esta funcion recibe mensajes a traves de un socket
-char *recibir(int socket_receptor);
+char *recibir_cl(int socket_receptor);
 
-//Esta funcion elimina el socket
+int iniciar_socket_server(char *ip, char *puerto);
+
+int escuchar_conexiones(int socketServidor);
+
 void cerrar_conexion(int socket_);
 
+int iniciar_socket_server(char *ip, char *puerto);
 
-#endif /* SOCKET_H_ */
+int enviar(int socket_emisor, char *mensaje_a_enviar, int *control);
+
+char *recibir(int socket_receptor, int *controlador);
+
+#endif /* KERNEL_PRUEBAS_SRC_SOCKET_CLIENT_H_ */
