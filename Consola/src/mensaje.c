2a82811ea2ab931fe2ea55b9f9f32ffedeef6f4f
caromuscari <caro.muscari@gmail.com>
2017-05-08 21:46:46 -0300

    arreglo de longitud del mensaje

--- Consola/src/mensaje.c
+++ Consola/src/mensaje.c
@@ -15,7 +15,7 @@ char *armar_mensaje(char *identificador, char *mensaje)
 	char *resultado = strdup(identificador);
 	char *payload_char = string_itoa(string_length(mensaje));
 	int size_payload = string_length(payload_char);
-	char *completar = string_repeat('0', 4 - size_payload);
+	char *completar = string_repeat('0', 10 - size_payload);
 
 	string_append(&resultado, completar);
 	string_append(&resultado, payload_char);
@@ -29,24 +29,24 @@ char *armar_mensaje(char *identificador, char *mensaje)
 //devuelve el header del mensaje
 char *get_header(char *mensaje)
 {
-	return string_substring(mensaje, 1, 1);
+	return string_substring(mensaje, 0, 1);
 }
 
 int comparar_header(char *identificador, char *mensaje)
 {
-	return strcmp(string_substring(mensaje, 1, 1), identificador);
+	return strcmp(string_substring(mensaje, 0, 1), identificador);
 }
 
-//devulve el codigo del mensaje
+//devuelve el codigo del mensaje
 int get_codigo(char *mensaje)
 {
-	return atoi(string_substring(mensaje, 2, 3));
+	return atoi(string_substring(mensaje, 1, 2));
 }
 
 //obtiene el mensaje
 char * get_mensaje(char *mensaje)
 {
-	char *payload = string_substring(mensaje, 2, 4);
+	char *payload = string_substring(mensaje, 3, 10);
 	int payload1 = atoi(payload);
-	return string_substring(mensaje, 7, payload1 - 1);
+	return string_substring(mensaje, 13, payload1 - 1);
 }
