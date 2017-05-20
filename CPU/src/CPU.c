33d76db4516a2adfa32b8f68b4100c781ba145ac
marumarino <marianamarino0696@hotmail.com>
2017-05-20 03:01:23 -0300

    CPU, subo mi version fea(muy fea) de cpu solo para mostrarles mañana, bai

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -7,12 +7,50 @@
 #include "socket.h"
 #include "log.h"
 #include "funcionesCPU.h"
+#include <parser/metadata_program.h>
 #include <commons/config.h>
+#include "cosas.h"
 
 int puertoK,puertoM;
 char *ipK;
 char *ipM;
 int sockKerCPU;
+static const char* facil_ansisop =
+		"begin\n"
+		"variables a, b\n"
+		"a = 3\n"
+		"b = 5\n"
+		"a = b + 12\n"
+		"end\n"
+		"\n";
+static const char* otro_ansisop =
+		"#!/usr/bin/ansisop\n"
+		"begin\n"
+		"# primero declaro las variables\n"
+		"variables a, b\n"
+		"a = 20\n"
+		"print a\n"
+		"end\n"
+		"\n";
+static const char* con_funcion_ansisop =
+		"#!/usr/bin/ansisop\n"
+		"begin variables a,g\n"
+		"a = 1\n"
+		"g <- doble a\n"
+		"prints g\n"
+		"variables i,b\n"
+		"i = 1\n"
+		":inicio_for\n"
+		"i = i + 1\n"
+		"jnz i inicio_for\n"
+		"#fuera del for\n"
+		"end\n"
+		"function doble\n"
+		"variables f\n"
+		"f = $0 + $0\n"
+		"return f\n"
+		"end\n"
+		"\n";
 
 
 void leerArchivoConfiguracion(char* argv);
@@ -20,25 +58,110 @@ void conexion_Kernel(int puertoK, char* ipK);
 
 int main(int argc, char *argv[])
 {
-
+	//char *programa =strdup(facil_ansisop);
+	//char *programa =strdup(otro_ansisop);
+	char *programa =strdup(con_funcion_ansisop);
 	crear_archivo_log("/home/utnso/CPUlog");
 	leerArchivoConfiguracion(argv[1]);
 	conexion_Kernel(puertoK, ipK);
 	//	conectarse con memoria
 	//	conexion_Memoria(puertoM,ipM);
 	int controlador = 0;
-	while(1){
+/*
+ *
+	while(chau){
 		void *buff = malloc (7);
+		char *idmensaje = malloc(2);
+		char *sizemensaje= malloc (4); int largomensaje;
 		escribir_log("Esperando mensajes del Kernel para ponerme a trabajar...",1);
 		recibir(sockKerCPU,&controlador,buff,7);
 		if(controlador!= 0){
 			escribir_log("error recibiendo mensaje del Kernel, bai",2);
 		}
 		printf("recibi:%s",buff);
+		memcpy(idmensaje,buff+1,2);
+
+		switch (atoi(idmensaje)){
+		case 7:
+			memcpy(sizemensaje,buff+3,4); largomensaje= atoi(sizemensaje);
+			free(sizemensaje);
+			char *mensajeEntero = malloc(largomensaje);
+			recibir(sockKerCPU,&controlador,mensajeEntero,largomensaje);
+			//t_pcb pcb = deserializar_pcb(mensajeEntero)
+			//procesar(pcb)
+
+		}
+
+
+	}
+*/
+	int pc = 0;
+	t_metadata_program *metadata= metadata_desde_literal(programa);
+
+	while(pc < metadata->instrucciones_size){
+		char *linea = string_substring(programa,metadata->instrucciones_serializado[pc].start,metadata->instrucciones_serializado[pc].offset);
+		analizadorLinea(linea,&funcionesTodaviaSirve,&funcionesKernelTodaviaSirve);
+		pc ++;
+		free(linea);
+	}
+
+
+	t_sentencia* sentencias =  armarIndiceCodgigo(programa);
+
+	int c;
+	for(c=0;sentencias[c].offset_fin != -1 && sentencias[c].offset_inicio != -1; c++){
+		printf("sentencias numero %d\n",c);
+		char *linea=string_substring(programa,sentencias[c].offset_inicio,sentencias[c].offset_fin);
+		printf("%s\n",linea);
+		free(linea);
+
 	}
 
 
 
+	printf("Cantidad de etiqueta:%d\n",metadata->cantidad_de_etiquetas);
+	printf("Cantidad de funciones:%d\n",metadata->cantidad_de_funciones);
+	//int m=0;
+	//while(m < metadata->cantidad_de_etiquetas + metadata->cantidad_de_funciones){
+		int largo1et = strlen(metadata->etiquetas);
+		char *etiqueta1 = strdup(metadata->etiquetas);
+		int largo2et = strlen((metadata->etiquetas)+largo1et+5);
+		char *etiqueta2 = strdup(metadata->etiquetas+5+largo1et);
+	//}
+		printf("1erET:%d%s,2daET:%d%s\n",largo1et,etiqueta1,largo2et,etiqueta2);
+	printf("Etiquetas:%s\n",metadata->etiquetas);
+	printf("Etiquetas size:%d\n",metadata->etiquetas_size);
+
+	t_dictionary *dic = armarIndiceEtiquetas(programa);
+	printf("espero size 2 y es size:%d\n",dictionary_size(dic));
+
+	int k= metadata_buscar_etiqueta("doble",metadata->etiquetas,metadata->etiquetas_size);
+	printf("buscaretiqueta DOBLE tpunteroinstruccion :%d\n",k);
+	k= metadata_buscar_etiqueta("inicio_for",metadata->etiquetas,metadata->etiquetas_size);
+		printf("buscaretiqueta INICIO_FOR tpunteroinstruccion :%d\n",k);
+
+	char *linea = string_substring(programa,sentencias[k].offset_inicio,sentencias[k].offset_fin);
+	printf("instruccion con el pc dado:%s\n",linea);
+	free(linea);
+
+	printf("Instruccion inicio:%d\n",metadata->instruccion_inicio);
+	printf("Instrucciones serializado/t_instrucciones.offset:%d\n",metadata->instrucciones_serializado->offset);
+	printf("Instrucciones serializado/t_instrucciones.start:%d\n",metadata->instrucciones_serializado->start);
+	printf("Instrucciones:%d\n",metadata->instrucciones_size);
+
+	int n=0;
+	while(n < metadata->instrucciones_size){
+		char *linea = string_substring(programa,metadata->instrucciones_serializado[n].start,metadata->instrucciones_serializado[n].offset);
+		printf("primer instruccion:%s\n",linea);
+		n++;
+		free(linea);
+
+	}
+
+	free(sentencias);
+free(programa);
+metadata_destruir(metadata);
+free(ipK);free(ipM);
 
 
 }
