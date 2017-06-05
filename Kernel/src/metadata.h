6898f9769fb178fb9e2e3d38794541db84afea25
marumarino <marianamarino0696@hotmail.com>
2017-06-05 20:37:17 -0300

    agrego serializacion/deserializacion del pcb completa, sin los cambios de estructuras no funciona asi que cambien estructuras gente Kernel

--- Kernel/src/metadata.h
+++ Kernel/src/metadata.h
@@ -8,9 +8,36 @@
 #ifndef METADATA_H_
 #define METADATA_H_
 
+typedef struct
+{
+	int offset_inicio;
+	int offset_fin;
+}t_sentencia;
+
+typedef struct
+{
+	int PID;
+	int PC;
+	int cant_pag;
+	int SP;
+	t_sentencia* in_cod;
+	char* in_et;
+	t_list* in_stack;  //lista de t_stack_element
+	int exit_code;
+}t_PCB;
+typedef struct
+{
+	char ID;
+	int pag;
+	int offset;
+	int size;
+}__attribute__((__packed__))t_memoria;
+
+
 t_sentencia* armarIndiceCodigo (char *codigoPrograma);
-void* armarIndiceEtiquetas(char *codigoPrograma);
-t_list* armarIndiceStack (char *codigoPrograma);
-void* serializarPCB(t_PCB pcb);
+char* armarIndiceEtiquetas(char *codigoPrograma);
+t_list* armarIndiceStack(char *codigoPrograma);
+char* serializarPCB_KerCPU (t_PCB,char *,int,int );
+t_PCB deserializarPCB_CPUKer (char* );
 
 #endif /* METADATA_H_ */
