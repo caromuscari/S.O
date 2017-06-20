d68fb8b182bf7c2090ce3a4d01f48975c577d9dd
utnso <christian.monasterios@gmail.com>
2017-06-20 19:40:07 -0300

    Funciones de la diva de me_MORIA

---
+++ Memoria/src/Memoria.h
@@ -0,0 +1,25 @@
+/*
+ * mensaje.h
+ *
+ *  Created on: 23/4/2017
+ *      Author: utnso
+ */
+
+#ifndef MEMORIA_H_
+#define MEMORIA_H_
+
+//Variables Globales
+/*
+char * memoria;
+
+//Estructuras administrativas
+
+typedef struct{
+	int asignado;
+	int pid;
+	int pag;
+} t_marco;
+
+t_marco* admMarcos;
+*/
+#endif /* MEMORIA_H_ */
