3d2fae8001366c22d8836379ef118b61d783318c
marumarino <marianamarino0696@hotmail.com>
2017-07-15 14:37:37 -0300

    d

--- compilartp.sh
+++ compilartp.sh
@@ -10,7 +10,7 @@ cd --
 
 git clone https://github.com/sisoputnfrba/ansisop-parser
 
-cd ansisop-parser/parse
+cd ansisop-parser/parser
 
 make all
 sudo make install
@@ -32,7 +32,7 @@ mv /home/utnso/tp-2017-1c-TodaviaSirve/evaluacion-final-esther/FS-ejemplo/SADICA
 
 cd /home/utnso/tp-2017-1c-TodaviaSirve/Consola/src
 
-gcc Consola.c escuchar_mensaje.c hilo_programa.c hilo_usuario.c loc.c mensaje.c parametros.c socket_cliente.c escuchar_mensaje.h hilo_prorama.h estructuras.h hilo_usuario.h log.h mensaje.h parametros.h socket_cliente.h -o Consola -lcommons -lpthread
+gcc Consola.c escuchar_mensaje.c hilo_programa.c hilo_usuario.c loc.c mensaje.c parametros.c socket_cliente.c escuchar_mensaje.h hilo_programa.h estructuras.h hilo_usuario.h log.h mensaje.h parametros.h socket_cliente.h -o Consola -lcommons -lpthread
 
 echo "Consola compilada, ejecutar normalmente"
 
@@ -48,7 +48,7 @@ echo "CPU compilada, ejecutar normalmente"
 
 cd /home/utnso/tp-2017-1c-TodaviaSirve/FileSystem/src
 
-gcc FileSystem.c archivos.c fsecundarias.c funciones.c log.c manejor_errores.c mensaje.c socket.c archivos.h estructuras.h fsecundarias.h funciones.h log.h manejo_errores.h mensaje.h socket.h -o FileSystem -lcommons -lpthread
+gcc FileSystem.c archivos.c fsecundarias.c funciones.c log.c manejar_errores.c mensaje.c socket.c archivos.h estructuras.h fsecundarias.h funciones.h log.h manejo_errores.h mensaje.h socket.h -o FileSystem -lcommons -lpthread
 
 cd --
 
@@ -56,7 +56,7 @@ echo "FileSystem compilado, ejecutar normalmente"
 
 cd /home/utnso/tp-2017-1c-TodaviaSirve/Kernel/src
 
-gcc Kernel.c configuracion.c consolaKernel.c consolaManager.c cpuManager.c filesystem.c log.c manejo_conexiones.c manejo_errores.c memoria.c memoria2.c mensaje_consola.c mensaje.c metadata.c planificador.c semaforos_vglobales.c socket.c configuracion.h consolaKernel.h consolaManager.h cpuManager.h estructuras.h filesystem.h log.h manejo_conexiones.h manejo_errores.h memoria.h memoria2.h mensaje_consola.h mensaje.h metadata.h planificador.h semaforos_vglobales.h socket.h -o Kernel -lcommons -lpthread -lparser-ansisop
+gcc Kernel.c configuracion.c consolaKernel.c consolaManager.c cpuManager.c fileSystem.c log.c manejo_conexiones.c manejo_errores.c memoria2.c mensaje_consola.c mensaje.c metadata.c planificador.c semaforos_vglobales.c socket.c configuracion.h consolaKernel.h consolaManager.h cpuManager.h estructuras.h fileSystem.h log.h manejo_conexiones.h manejo_errores.h memoria2.h mensaje_consola.h mensaje.h metadata.h planificador.h semaforos_vglobales.h socket.h -o Kernel -lcommons -lpthread -lparser-ansisop
 
 cd --
 
